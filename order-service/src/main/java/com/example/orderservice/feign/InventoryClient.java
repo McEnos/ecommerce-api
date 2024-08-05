5062dad11737d1da94dc0d254057ce1458b24ca0
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2024-08-05 07:20:42 +0300

    Added eureka server for service discovery

--- order-service/src/main/java/com/example/orderservice/feign/InventoryClient.java
+++ order-service/src/main/java/com/example/orderservice/feign/InventoryClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
+@FeignClient(name= "${inventory.name}")
 public interface InventoryClient {
-    @GetMapping("/api/inventory")
+    @GetMapping("/${inventory.name}/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
